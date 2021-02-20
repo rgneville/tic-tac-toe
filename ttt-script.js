84cdcfbd0793d4936fe58e2188b6393f0080626a
rgneville <rgneville91@gmail.com>
2021-02-20 18:49:30 +0000

    First version - buggy

--- ttt-script.js
+++ ttt-script.js
@@ -1,5 +1,17 @@
+const ctr = document.querySelector('#container');
+const startButton = document.querySelector('#start');
+const scorebox = document.querySelector('#scorebox');
+const player1Element = document.querySelector('#player-1-name');
+const player2Element = document.querySelector('#player-2-name');
+
+startButton.addEventListener('click', () => {
+    gameHandlerModule.runGame();
+});
+
+//current issues: checkWin not working, checkWinTest not working, can't get currentPlayer object into generateGrid event listener so the right marker goes into the grid.
+
 const gameboardModule = (function() {
-    let _boardArray = ["", "", "", "", "", "", "", "", ""];
+    let _boardArray = ["X", "", "", "X", "", "", "X", "", ""];
 
     function _checkWin () {
         //checking all 8 possible winning combinations in _boardArray
@@ -21,47 +33,76 @@ const gameboardModule = (function() {
         _checkWin();
     }
 
+    //test function
+    function checkWinTest () {
+        if (_boardArray[0] === _boardArray[3] === _boardArray[6]) {
+                return true
+            } else {
+                return false
+            }
+    }
+
     function generateGrid () {
         for (let i = 0; i < _boardArray.length; i++) {
-            const container = document.querySelector('#container');
-            const gridBox = document.createElement('div');
-        //set container to grid, add a CSS class for each gridBox element, gridBox.innerHTML = relevant aappend to container, add i as id, add event listener, 
+            const markerBox = document.createElement('div');
+            markerBox.classList.add('gridbox');
+            markerBox.id = `${i}`;
+            scorebox.innerHTML = "Game started";
+            markerBox.addEventListener('click', (e) => {
+                if (_boardArray[i] != "") {
+                    e.stopPropagation();
+                };
+                markerBox.innerHTML = "x";
+                const winbool = checkWin();
+                if (winbool === false) {
+                    gameHandlerModule.changePlayer();
+                } else if (winbool === true) {
+                    scorebox.innerHTML = "Winner winner chicken dinner!";
+                };
+            });
+            ctr.appendChild(markerBox);
+        //set container to grid, add a CSS class for each gridBox element, append to container, add i as id, add event listener, 
         //event listener should add currentplayer.marker to array and check for win, change player if no win
         }
     }
 
     return {
+        generateGrid: generateGrid,
         checkWin: checkWin,
-        generateGrid: generateGrid
+        checkWinTest: checkWinTest
     };
 
 })();
 
 const playerFactory = (name, marker) => {
-    const printMarker = () => console.log(marker);
+    const printMarker = () => marker;
     return { name, marker, printMarker };
 };
 
 const gameHandlerModule = (function () {
-    let currentPlayer = firstPlayer.name;
+    let firstPlayer = {};
+    let secondPlayer = {};
+    let currentPlayer = {};
 
     function createPlayers () {
-        const firstPlayer = playerFactory("Steve", "X")
-        const secondPlayer = playerFactory("Alice", "X")
+        let firstPlayerName = player1Element.value;
+        let secondPlayerName = player2Element.value;
+        firstPlayer = playerFactory(firstPlayerName, "X");
+        secondPlayer = playerFactory(secondPlayerName, "O");
+        currentPlayer = firstPlayer;
     }
 
     function changePlayer () {
-        if (currentPlayer === firstPlayer.name) {
-            currentPlayer = secondPlayer.name;
-        } else if (currentPlayer === secondPlayer.name) {
-            currentPlayer = firstPlayer.name;
+        if (currentPlayer === firstPlayer) {
+            currentPlayer = secondPlayer;
+        } else if (currentPlayer === secondPlayer) {
+            currentPlayer = firstPlayer;
         }
     }    
     
     function runGame () {
-        //runGame on clicking start button
         createPlayers();
-        generateGrid();
+        gameboardModule.generateGrid();
     }
 
     return {
@@ -70,3 +111,6 @@ const gameHandlerModule = (function () {
     }
 
 })();
+
+let winTest = checkWinTest();
+console.log(winTest);
